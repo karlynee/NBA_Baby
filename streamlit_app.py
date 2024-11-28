fe181162d194b9fd09b95c1c34da9ab6543618f8
karlynee <karlyne.malonga@gmail.com>
2024-11-28 11:14:53 +0000

    rebase with baseline

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,34 @@
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
+from google.oauth2 import id_token
+from google.auth.transport import requests
 import pandas as pd
 import pandas_gbq
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+@st.cache_data(ttl=600)
+def run_query(_client,_query):
+    query_job = _client.query(_query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-# Exemple de requête
-sql = """
- SELECT * FROM nba-baby-442813.Players.players_advanced_22_23;
-"""
-project_id = 'nba-baby-442813'
-df = pandas_gbq.read_gbq(sql, project_id=project_id)
+def main():
+    st.title("NBA BABYYYY")
+    # Create API client.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bigquery.Client(credentials=credentials)
+    sql = """
+    SELECT * FROM nba-baby-442813.Players.players_advanced_22_23_24;
+    """
+    rows = run_query(client,sql)
 
-
-st.write(
-    df
-)
-
+    # Print results.
+    st.write("Some wise words from Shakespeare:")
+    st.write(rows)
+    
+if __name__ == "__main__":
+    main()
