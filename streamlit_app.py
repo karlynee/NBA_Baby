34ee213a75fa3c0c71c9d4ff651493fb116fff00
karlynee <karlyne.malonga@gmail.com>
2024-11-28 11:14:33 +0000

    rebase

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,24 @@
 import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 import pandas as pd
+import pandas_gbq
 
-st.write(1234)
-st.write(
-    pd.DataFrame(
-        {
-            "first column": [1, 2, 3, 4],
-            "second column": [10, 20, 30, 40],
-        }
-    )
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
 )
-sql = "SELECT * FROM nba-baby-442813.Teams.total_stats_23_24;"
-#df_stats2324 = pd.read_gbq(sql,project_id="nba-baby-442813",dialect="standard")
-#df_stats2324.head()
+client = bigquery.Client(credentials=credentials)
+
+# Exemple de requête
+sql = """
+ SELECT * FROM nba-baby-442813.Players.players_advanced_22_23;
+"""
+project_id = 'nba-baby-442813'
+df = pandas_gbq.read_gbq(sql, project_id=project_id)
+
+
+st.write(
+    df
+)
 
